28e4b50f08d996c42a0d969e9fcc807ea3f15b84
Erkaikin <erkai30091995@gmail.com>
2022-12-27 00:55:22 -0800

    added outputs to root module

--- tf/modules/ec2/outputs.tf
+++ tf/modules/ec2/outputs.tf
@@ -4,5 +4,5 @@ output "instance_id" {
 
 output "instance_public_ip" {
    description = "Public IP of EC2 instance"
-   value       = try(aws_instance.ec2.*.public_ip, "")
+   value       = aws_instance.ec2.*.public_ip
 }
--- tf/outputs.tf
+++ tf/outputs.tf
@@ -8,9 +8,16 @@ output "instance_id" {
   value       = module.ec2.instance_id
 }
  
-output "instance_public_ip" {
+output "instance_public_ip1" {
    description = "Public IP of EC2 instance"
-   value       = module.ec2.instance_public_ip
+   value       = module.ec2.instance_public_ip[0]
 
 
 }
+output "instance_public_ip2" {
+   description = "Public IP of EC2 instance"
+   value       = module.ec2.instance_public_ip[1]
+
+
+}
+
